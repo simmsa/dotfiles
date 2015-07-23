537894f1526ea8495a1a49acd422862ddf5e7dde
Andrew Simms <simms.andrew@gmail.com>
2015-07-23 09:35:22 -0600

    Alias: Sort common aliases in alphabetical order.

--- aliases.zsh
+++ aliases.zsh
@@ -10,38 +10,39 @@ if type "nvim" > /dev/null; then
 fi
 
 source ~/dotfiles/local_aliases.zsh
-alias md="mkdir -p"
+
+alias ....="cd ../../.."
 alias bk="cd .."
-alias py3="python3"
-alias edz="vim ~/dotfiles/zshrc"
+alias djrs="python manage.py runserver 8888"
 alias eda="vim ~/dotfiles/aliases.zsh"
 alias edla="vim ~/dotfiles/local_aliases.zsh"
-alias scz="source ~/.zshrc"
-alias djrs="python manage.py runserver 8888"
 alias edvim="vim ~/.nvim/nvimrc"
-alias pry="pry --simple-prompt"
-alias rs="rails s -p 8888"
+alias edz="vim ~/dotfiles/zshrc"
 alias gemdir="gem env gemdir"
-alias x="exit"
 alias gpp="g++"
 alias j="jump"
+alias l="ls -lh"
+alias md="mkdir -p"
 alias objcopy="gobjcopy"
 alias objdump="gobjdump"
-
-mcd() {
-    mkdir -p "$1" && cd "$1"
-}
-alias tmux_rename_window='tmux rename-window $(basename $(pwd))'
 alias plz='sudo $(fc -ln -1)'
-# Show Colors
+alias pry="pry --simple-prompt"
+alias py3="python3"
+alias rs="rails s -p 8888"
+alias scz="source ~/.zshrc"
 alias showcolors='for i in {0..255}; do echo -e "\e[38;05;${i}m${i}"; done | column -c 150 -s '"'"'  '"'"'; echo -e "\e[m"'
-alias ....="cd ../../.."
+alias tmux_rename_window='tmux rename-window $(basename $(pwd))'
+alias x="exit"
 
-alias l="ls -lh"
 m () {
     make f=$1
 }
+
 mc () {
     make clean f=$1
+}
+
+mcd() {
+    mkdir -p "$1" && cd "$1"
 }
 
